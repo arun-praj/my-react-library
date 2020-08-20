937f1d65c960de1f1ed619fa5f3e39a5c0f8403a
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-20 09:44:49 +0545

    [Add] Dropdown library

---
+++ Dropdown/Dropdown.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Dropdown.scss";
+
+export const Dropdown = (props) => {
+   return (
+      <div>
+         <div className='dropdown'>
+            <div className='dropdown__button'>{props.button}</div>
+            <div className='dropdown__box'>
+               <div className='dropdown__box__container'>{props.children}</div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export const Dropgroups = (props) => {
+   return <div className='dropdown__group'>{props.children}</div>;
+};
+export const DropLink = (props) => {
+   return (
+      <Link style={props.style} className='dropdown__link' to={props.to}>
+         {props.value}
+      </Link>
+   );
+};
